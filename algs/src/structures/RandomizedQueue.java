90576a193963d034cbddd2eb168c2fcac58802ff
Jared Kuemmerle <jared.kuemmerle@yahoo.com>
2014-02-14 14:52:02 -0800

    Randomized Queue interface

---
+++ algs/src/structures/RandomizedQueue.java
@@ -0,0 +1,68 @@
+package structures;
+
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    /**
+     * construct an empty randomized queue
+     */
+    public RandomizedQueue() {
+        
+    }
+
+   /**
+    * is the queue empty?
+    * @return
+    */
+    public boolean isEmpty() {
+       
+    }
+   
+    /**
+     * return the number of items on the queue
+     * @return
+     */
+    public int size() {
+        
+    }
+   
+    /**
+     * add the item
+     * @param item
+     */
+    public void enqueue(Item item) {
+        
+    }
+  
+    /**
+     * delete and return a random item
+     * @return
+     */
+    public Item dequeue() {
+        
+    }
+   
+    /**
+     * return (but do not delete) a random item
+     * @return
+     */
+    public Item sample() {
+        
+    }
+    
+    /**
+     * return an independent iterator over items in random order
+     */
+    public Iterator<Item> iterator() {
+        
+    }
+   
+    /**
+     * unit testing.
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+}
