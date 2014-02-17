b87a908d254382f4038dc05fc64076164576c96f
Jared Kuemmerle <jared.kuemmerel@yahoo.com>
2014-02-17 07:48:05 -0800

    bug fix

--- algs/src/structures/RandomizedQueue.java
+++ algs/src/structures/RandomizedQueue.java
@@ -5,18 +5,22 @@ import java.util.NoSuchElementException;
 
 import edu.princeton.cs.introcs.StdRandom;
 
+/**
+ * Randomized Queue
+ * @param <Item>
+ */
 public class RandomizedQueue<Item> implements Iterable<Item> {
-    
+
     /**
      * Size of the deque.
      */
     private static final int CAPACITY_START = 4;
-    
+
     /**
      * Randomized queue items.
      */
     private Item[] s;
-    
+
     /**
      * Size of the randomized queue.
      */
@@ -26,44 +30,46 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
      * construct an empty randomized queue.
      */
     public RandomizedQueue() {
-        
+
         s = (Item[]) new Object[CAPACITY_START];
     }
 
    /**
     * is the queue empty?
-    * @return
+    * @return boolean
     */
     public boolean isEmpty() {
-       
+
         return N == 0;
     }
-   
+
     /**
      * return the number of items on the queue.
-     * @return
+     * @return int
      */
     public int size() {
-        
+
         return N;
     }
-   
+
     /**
      * add the item
      * @param item the item
      */
     public void enqueue(Item item) {
-        
-        conditionalResizeUp();
+
+        if (N == s.length) {
+            resizeUp(N * 2);
+        }
         s[N++] = item;
     }
-  
+
     /**
      * delete and return a random item.
-     * @return
+     * @return Item
      */
     public Item dequeue() {
-        
+
         int removedIndex = StdRandom.uniform(N);
         Item item = s[removedIndex];
         if (N > 0 && N < s.length / 4) {
@@ -73,62 +79,60 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         }
         return item;
     }
-   
+
     /**
      * return (but do not delete) a random item.
-     * @return
+     * @return Item
      */
     public Item sample() {
-        
+
         int sampledIndex = StdRandom.uniform(N);
         return s[sampledIndex];
     }
-    
+
     /**
      * return an independent iterator over items in random order.
+     * @return RandomizedQueueIterator;
      */
     public Iterator<Item> iterator() {
-        
+
         return new RandomizedQueueIterator();
     }
-   
+
     /**
-     * Conditionally grow the array if size is at capacity.
+     * Grow the array.
      * @param capacity the capacity.
      */ 
-    private void conditionalResizeUp() {
+    private void resizeUp(int capacity) {
 
-        if (N == s.length) {
-            int capacity = N * 2;
-            Item[] copy = (Item[]) new Object[capacity];
-            for (int i = 0; i < N; i++) {
-                copy[i] = s[i];
-            }
-            s = copy;
+        Item[] copy = (Item[]) new Object[capacity];
+        for (int i = 0; i < N; i++) {
+            copy[i] = s[i];
         }
+        s = copy;
     }
 
-    
     /**
-     * Resize the array.
-     * @param capacity
+     * Shrink the array and remove an index.
+     * @param removedIndex the removed index
+     * @param capacity the capacity
      */
     private void resizeDown(int removedIndex, int capacity) {
 
-        int i;
+        int i, lastIndex;
         Item[] copy = (Item[]) new Object[capacity];
         for (i = 0; i < removedIndex; i++) {
             copy[i] = s[i];
         }
-        if (removedIndex != N - 1) {
-            for (i = removedIndex + 1; i < N; i++) {
-                copy[i] = s[i];
-            }
+        lastIndex = N - 1;
+        for (i = removedIndex; i < lastIndex; i++) {
+            copy[i] = s[i + 1];
         }
+
         s = copy;
         --N;
     }
-    
+
     /**
      * Resize the array.
      * @param removedIndex
@@ -139,6 +143,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         for (int i = removedIndex; i < lastIndex; i++) {
             s[i] = s[i + 1];
         }
+        --N;
     }
     
     /**
@@ -146,16 +151,17 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
      */
     private class RandomizedQueueIterator implements Iterator<Item> {
 
-    	/**
-    	 * the index of the current element.
-    	 */
+        /**
+         * the index of the current element.
+         */
         private int current = N;
-        
+
         /**
          * Is there a next element?
+         * @return boolean
          */
         public boolean hasNext() {
-            
+
             return current > 0;
         }
 
@@ -163,12 +169,13 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
          * Not implemented.
          */
         public void remove() {
-            
+
             throw new UnsupportedOperationException();
         }
 
         /**
          * Return the next item.
+         * @return Item
          */
         public Item next() {
 
@@ -178,24 +185,25 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
             return s[--current];
         }
     }
-    
+
     /**
      * unit testing.
      * @param args
      */
     public static void main(String[] args) {
-        
+
         int i;
         RandomizedQueue<String> r = new RandomizedQueue<String>();
         String testStr = "foo";
 
         for (i = 0; i < 20; i++) {
-        	r.enqueue(testStr);
+            r.enqueue(testStr);
         }
-        
+        System.out.println(r.size());
         for (i = 0; i < 20; i++) {
-        	String foo = r.dequeue();
-        	System.out.println(foo);
+            String foo = r.dequeue();
+            System.out.println(foo);
         }
+        System.out.println(r.size());
     }
 }
