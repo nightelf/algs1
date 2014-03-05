53fc914f5e71e3be4b7de2d32d6e953b10f409a0
Jared Kuemmerle <jared.kuemmerel@yahoo.com>
2014-03-05 06:56:09 -0800

    more board dev

--- algs/src/puzzle8/Board.java
+++ algs/src/puzzle8/Board.java
@@ -2,34 +2,40 @@ package puzzle8;
 
 public class Board {
 
-	final private int[][] blocks;
-	
-	final private int[][] goal;
-	
-	final private int N;
-	
+    final private int[][] blocks;
+    
+    final private int[][] goal;
+    
+    final private int N;
+    
     /**
      * Construct a board from an N-by-N array of blocks
      * (where blocks[i][j] = block in row i, column j)
      */
     public Board(int[][] blocks) {
         
-    	this.blocks = blocks;
-    	this.goal = new int[this.blocks.length][this.blocks.length];
-    	N = this.blocks.length;
+        this.blocks = blocks;
+        this.goal = new int[this.blocks.length][this.blocks.length];
+        N = this.blocks.length;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++)
                 goal[i][j] = N * i + j + 1;
         }
+        goal[N - 1][N - 1] = 0;
     }
 
-    private int[] getIndicies(key) {
-    	
-    	int[] ret = new int[2];
-    	ret[0] = key / N;
-    	ret[1] = key / N;
-    	
-    	return ret
+    private int[] getIndicies(int number) {
+
+        int[] ret = new int[2];
+        if (number == 0) {
+            ret[0] = N - 1;
+            ret[1] = N - 1;
+        } else {
+            ret[0] = (number - 1) / N;
+            ret[1] = number % N;
+            ret[1] = ret[1] == 0 ? N - 1 : ret[1] - 1;
+        }
+        return ret;
     }
     
     /**
@@ -37,7 +43,7 @@ public class Board {
      * @return
      */
     public int dimension() {
-    	
+        
         return N;
     }
     
@@ -47,6 +53,15 @@ public class Board {
      */
     public int hamming() {
         
+        int hamming = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (goal[i][j] != blocks[i][j]) {
+                    hamming++;
+                }
+            }
+        }
+        return hamming;
     }
     
     /**
@@ -54,7 +69,15 @@ public class Board {
      * @return
      */
     public int manhattan() {
-        
+        int x, y;
+        int manhattan = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+            	
+                	manhattan += (i - x) + (j - y);
+                
+            }
+        }
     }
     
     /**
