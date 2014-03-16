f29f2c2e66c04bb75674c79064c1d35a8e818bbe
Jared Kuemmerle <jared.kuemmerel@yahoo.com>
2014-03-16 08:16:39 -0700

    Implemented Nearest

--- algs/src/structures/KdTree.java
+++ algs/src/structures/KdTree.java
@@ -18,6 +18,11 @@ public class KdTree {
      * The size of the KdTree
      */
     private int N = 0;
+    
+    /**
+     * A found Node. Used in nearest search.
+     */
+    private Node found;
 
     /**
      * construct an empty set of points.
@@ -55,6 +60,12 @@ public class KdTree {
         root.isYvector = true;
     }
     
+    /**
+     * Insert
+     * @param parent
+     * @param child
+     * @return Node
+     */
     private Node insert (Node parent, Node child) {
 
         if (null == parent) {
@@ -94,12 +105,17 @@ public class KdTree {
     }
     
 
-    // value associated with the given key in subtree rooted at x; null if no such key
+    /**
+     * Value associated with the given key in subtree rooted at x;
+     * null if no such key.
+     * @param p
+     * @return
+     */
     private Point2D get(Point2D p) {
-        
+
         Node x = root;
         Node find = new Node(p);
-        
+
         while (x != null) {
 
             int cmp = x.compare(find);
@@ -124,9 +140,9 @@ public class KdTree {
      * @return
      */
     public Iterable<Point2D> range(RectHV rect) {
-        
+
         ArrayList<Point2D> list = new ArrayList<Point2D>();
-        
+
         keys(root, list, rect);
         return list;
     }
@@ -149,16 +165,42 @@ public class KdTree {
             keys(n.left, list, rect);
             keys(n.right, list, rect);
         }
-    } 
-    
+    }
+
     /**
      * a nearest neighbor in the set to p; null if set is empty.
      * @param p point
      * @return
      */
     public Point2D nearest(Point2D p) {
-        return p; // TODO implement
+        
+        found = root;
+        Node nearme = new Node(p);
+        nearestSearch(root, nearme);
+        return found.point;
     }
+
+    /**
+     * Recursively search for the nearest neighbor.
+     * @param n
+     * @param found
+     * @param nearme
+     */
+    private void nearestSearch(Node n, Node nearme) {
+
+        if (n == null) return;
+        if (nearme.distanceSquaredTo(n) < nearme.distanceSquaredTo(found)) {
+            found = n;
+        }
+
+        int cmp = n.compareNearest(nearme);
+        if (cmp < 0) nearestSearch(n.left, nearme); 
+        else if (cmp > 0) nearestSearch(n.right, nearme);
+        else {
+            nearestSearch(n.left, nearme);
+            nearestSearch(n.right, nearme);
+        }
+    } 
     
     /**
      * Node.
@@ -204,14 +246,14 @@ public class KdTree {
 
             return point.x() == n.point.x() && point.y() == n.point.y();
         }
-        
+
         /**
          * Compare 2 this Node and another.
          * @param n
          * @return
          */
         public int compare(Node n) {
-            
+
             if (isYvector) {
                 if (point.x() > n.point.x()) return -1;
                 if (point.x() < n.point.x()) return 1;
@@ -229,7 +271,7 @@ public class KdTree {
          * @return
          */
         public int compare(RectHV rect) {
-            
+
             if (isYvector) {
                 if (point.x() > rect.xmax()) return -1;
                 if (point.x() < rect.xmin()) return 1;
@@ -240,14 +282,60 @@ public class KdTree {
                 else return 0;
             }
         }
+
+        /**
+         * Compare if the nearest point is in the box.
+         * @param nearme
+         * @return
+         */
+        public int compareNearest(Node nearme) {
+
+            double dNearest = found.distanceSquaredTo(nearme);
+            double dRect = distanceSquaredToRect(nearme);
+            int cmp = compare(nearme);
+            if (dNearest < dRect) {
+                if (cmp < 0) return -1;
+                else if(cmp > 0) return 1;
+            }
+            return 0;
+        }
+
+        /**
+         * Distance squared from p to the Node point.
+         * @param p a point2d
+         * @return double
+         */
+        public double distanceSquaredTo(Node n) {
+
+            double dy = point.y() - n.point.y();
+            double dx = point.x() - n.point.x();
+            return dx*dx + dy*dy;
+        }
         
+        /**
+         * 
+         * @param n
+         * @return
+         */
+        public double distanceSquaredToRect(Node n) {
+
+            double dp;
+            if (isYvector) {
+                dp = point.x() - n.point.x();
+            } else {
+                dp = point.y() - n.point.y();
+            }
+
+            return dp*dp;
+        }
+
         /**
          * Set the orientation of the right Node.
          */
         public void setRightOrientation() {
             right.isYvector = isYvector ? false : true; 
         }
-        
+
         /**
          * Set the orientation of the Left Node.
          */
@@ -263,57 +351,62 @@ public class KdTree {
     public static void main(String[] args) {
 
         KdTree kd = new KdTree();
-        
+
         Point2D point1 = new Point2D(0.5, 0.5);
         kd.insert(point1);
         assert kd.size() == 1;
         assert kd.contains(point1);
         assert false == kd.contains(new Point2D(0.7, 0.7));
-        
+
         Point2D point2 = new Point2D(0.4, 0.4);
         kd.insert(point2);
         assert kd.size() == 2;
         assert kd.contains(point2);
-        
+
         Point2D point3 = new Point2D(0.5, 0.5);
         kd.insert(point3);
         assert kd.size() == 2;
         assert kd.contains(point3);
-        
+
         Point2D point4 = new Point2D(0.5, 0.4);
         kd.insert(point4);
         assert kd.size() == 3;
         assert kd.contains(point4);
-        
+
         Point2D point5 = new Point2D(0.6, 0.5);
         kd.insert(point5);
         assert kd.size() == 4;
         assert kd.contains(point5);
         assert false == kd.contains(new Point2D(0.6, 0.4));
         assert false == kd.contains(new Point2D(0.6, 0.3));
-        
+
         //RectHV rect = new RectHV(0.55, 0.35, 0.65, 0.45);
         RectHV rect = new RectHV(0.0, 0.0, 1.0, 1.0);
         for (Point2D thePoint : kd.range(rect)) {
-        	System.out.println(thePoint.toString());
+            System.out.println(thePoint.toString());
         }
-        
+
         System.out.println();
         RectHV rect1 = new RectHV(0.45, 0.35, 0.55, 0.55);
         for (Point2D thePoint : kd.range(rect1)) {
-        	System.out.println(thePoint.toString());
+            System.out.println(thePoint.toString());
         }
-        
+
         System.out.println();
         RectHV rect2 = new RectHV(0.35, 0.35, 0.55, 0.45);
         for (Point2D thePoint : kd.range(rect2)) {
-        	System.out.println(thePoint.toString());
+            System.out.println(thePoint.toString());
         }
-        
+
         System.out.println();
         RectHV rect3 = new RectHV(0.55, 0.45, 0.6, 0.5);
         for (Point2D thePoint : kd.range(rect3)) {
-        	System.out.println(thePoint.toString());
+            System.out.println(thePoint.toString());
         }
+        
+
+        assert point4.equals(kd.nearest(new Point2D(0.49, 0.4)));
+        assert point5.equals(kd.nearest(new Point2D(0.55, 0.55)));
+        assert point1.equals(kd.nearest(new Point2D(0.54, 0.56)));
     }
 }
