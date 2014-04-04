c65c65c45772ffcf5501c8b936b91e80d40cde92
Jared Kuemmerle <jared.kuemmerel@yahoo.com>
2014-04-03 23:07:05 -0700

    changed structure

--- algs/src/graph/SeamCarver.java
+++ algs/src/graph/SeamCarver.java
@@ -15,14 +15,9 @@ public class SeamCarver {
     final private static double EDGE_ENERGY =  195075.0;
     
     /**
-     * The picture.
-     */
-    private Picture pic;
-    
-    /**
      * The energy array.
      */
-    private double[][] energy;
+    private pixel[][] pixels;
     
     /**
      * Constructor.
@@ -30,15 +25,14 @@ public class SeamCarver {
      */
     public SeamCarver(Picture picture) {
         
-        pic = new Picture(picture);
-        int width = pic.width();
-        int height = pic.height();
-        energy = new double[height][width];
+        int width = picture.width();
+        int height = picture.height();
+        pixels = new pixel[height][width];
         
         // fill energy grid
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                energy[i][j] = energy(j, i);
+                pixels[i][j] = new pixel(picture.get(j, i), energy(j, i));
             }
         }
     }
@@ -49,6 +43,16 @@ public class SeamCarver {
      */
     public Picture picture() {
         
+        int width = pixels[0].length;
+        int height = pixels.length;
+        
+        Picture pic = new Picture(width, height);
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                pic.set(j, i, pixels[i][j].color);
+            }
+        }
+        
         return pic;
     }
 
@@ -58,7 +62,7 @@ public class SeamCarver {
      */
     public int width() {
         
-        return pic.width();
+        return pixels[0].length;
     }
 
     /**
@@ -67,7 +71,7 @@ public class SeamCarver {
      */
     public int height() {
         
-        return pic.height();
+        return pixels.length;
     }
 
     /**
@@ -78,12 +82,20 @@ public class SeamCarver {
      */
     public double energy(int x, int y) {
 
-        if (x == 0 || y == 0 || x == energy[0].length || y == energy.length) {
+        if (x == 0 || y == 0 || x == pixels[0].length || y == pixels.length) {
             return EDGE_ENERGY;
         }
         
-        double horizontal = colorDiff(pic.get(x - 1, y), pic.get(x + 1, y));
-        double vertical = colorDiff(pic.get(x, y - 1), pic.get(x, y + 1));
+        double horizontal = colorDiff(
+            pixels[x - 1][y].color,
+            pixels[x + 1][y].color
+        );
+        
+        double vertical = colorDiff(
+            pixels[x][y - 1].color,
+            pixels[x][y + 1].color
+        );
+
         return horizontal + vertical;
     }
 
@@ -95,10 +107,10 @@ public class SeamCarver {
      */
     private double colorDiff(Color p1, Color p2) {
         
-        double red = Math.pow(p2.getRed() - p1.getRed(), 2);
-        double green = Math.pow(p2.getGreen() - p1.getGreen(), 2);
-        double blue = Math.pow(p2.getBlue() - p1.getBlue(), 2);
-        return red + green + blue;
+        double red = p2.getRed() - p1.getRed();
+        double green = p2.getGreen() - p1.getGreen();
+        double blue = p2.getBlue() - p1.getBlue();
+        return red * red + green * green + blue * blue;
     }
     
     /**
@@ -128,8 +140,12 @@ public class SeamCarver {
     public void removeHorizontalSeam(int[] a) {
 
         // remove Seam from energy matrix
+        int newLength = pixels[0].length - 1;
         for (int i = 0; i < a.length; i++) {
-            energy[a[i]][i] = -1;
+            pixel[] temp = new pixel[newLength];
+            System.arraycopy(pixels[a[i]], 0, temp, 0, i);
+            System.arraycopy(pixels[a[i]], i + 1, temp, i, newLength - i);
+            pixels[a[i]] = temp;
         }
     }
 
@@ -140,8 +156,39 @@ public class SeamCarver {
     public void removeVerticalSeam(int[] a) {
         
         // remove Seam from energy matrix
+        int newLength = pixels[0].length - 1;
         for (int i = 0; i < a.length; i++) {
-            energy[i][a[i]] = -1;
+            pixel[] temp = new pixel[newLength];
+            System.arraycopy(pixels[i], 0, temp, 0, a[i]);
+            System.arraycopy(pixels[i], a[i] + 1, temp, a[i], newLength - a[i]);
+            pixels[i] = temp;
+        }
+    }
+
+    /**
+     * Pixel contains color and energy information for a given pixel.
+     */
+    private class pixel {
+        
+        /**
+         * Representation of RGB color.
+         */
+        public Color color;
+        
+        /**
+         * The calculated energy.
+         */
+        public double energy;
+        
+        /**
+         * Constructor.
+         * @param color the color
+         * @param energy the energy
+         */
+        public pixel(Color color, double energy) {
+            
+            this.color = color;
+            this.energy = energy;
         }
     }
 }
